748a1d81cd89f653057c9f7e2266064ecec65ee3
Ivan Sizov <wuller1@gmail.com>
2022-11-13 22:36:08 +1000

    edited Task07 and Task05

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите целое число: ");
 var number = Convert.ToInt32(Console.ReadLine());
 
-for (var i = -number; i < number + 1; i++)
+for (var i = -number; i <= number; i++)
 {
     if (i != number)
     {
