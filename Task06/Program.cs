3b02a5fa86f5933f6bbc2d89054f131ce1e73825
Ivan Sizov <wuller1@gmail.com>
2022-11-13 23:01:09 +1000

    Solved Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число: ");
+var number = Convert.ToInt32(Console.ReadLine());
+string answer = number % 2 == 0 ? "является" : "не является";
+Console.WriteLine($"Число {number} {answer} четным");
