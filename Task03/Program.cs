98b0fc3ea1420fb6c1c912b96a5baf679c3b4984
Ivan Sizov <wuller1@gmail.com>
2022-11-13 20:21:33 +1000

    Solved Task05

---
+++ Task03/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число дня недели (от 1 до 7): ");
+int dayOfWeekNumber = Convert.ToInt32(Console.ReadLine());
+string[] daysOfWeek = new string[] {
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+    "Воскресенье",
+};
+
+if (dayOfWeekNumber >= 1 && dayOfWeekNumber <= 7)
+{
+    Console.WriteLine(daysOfWeek[dayOfWeekNumber - 1]);
+}
+else
+{
+    Console.WriteLine("Такого дня недели не существует");
+}
---
+++ Task05/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите целое число: ");
+var number = Convert.ToInt32(Console.ReadLine());
+
+for (var i = -number; i < number + 1; i++)
+{
+    if (i != number)
+    {
+        Console.Write($"{i}, ");
+    }
+    else
+    {
+        Console.Write($"{i}");
+    }
+}
+Console.WriteLine();
