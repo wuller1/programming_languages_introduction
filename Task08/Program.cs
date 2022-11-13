5b1029ebad01596d454691a06e3826755f7dff91
Ivan Sizov <wuller1@gmail.com>
2022-11-13 23:10:09 +1000

    Solved Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+var number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    if (i % 2 == 0 && i != number && i != number - 1)
+    {
+        Console.Write($"{i}, ");
+    }
+    else if (i % 2 == 0)
+    {
+        Console.Write($"{i}");
+    }
+}
+Console.WriteLine();
