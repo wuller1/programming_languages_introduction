e0e7091b607459a09f2e2210a04a95817449ea08
Ivan Sizov <wuller1@gmail.com>
2022-11-13 20:15:55 +1000

    solved Task01 and Task03

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Введите целое число: ");
+var number = Convert.ToInt64(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"Число {number} в квадрате равно: {number * number}");
--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Введите первое число: ");
+var number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Введите второе число: ");
+var number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+if (number2 * number2 == number1)
+{
+    Console.WriteLine("Первое число является квадратом второго числа");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго числа");
+}
+
