f5c665f99ada6b5bb6af126851a0c253f9b6bd96
Ivan Sizov <wuller1@gmail.com>
2022-11-19 01:29:32 +1000

    solved Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,20 @@
+﻿int number = -1;
+bool isNumber = false;
+
+while (!isNumber || number < 0)
+{
+    Console.Write("Введите число: ");
+    isNumber = Int32.TryParse(Console.ReadLine(), out number);
+}
+
+string CubeTable(int num)
+{
+    string table = "";
+    for (int i = 1; i <= num; i++)
+    {
+        table += $"{i}\t{i * i * i}\n";
+    }
+    return table;
+}
+
+Console.WriteLine(CubeTable(number));
