d71b180e2b013a86a27339da133f39ae3755d422
Ivan Sizov <wuller1@gmail.com>
2022-11-19 00:47:43 +1000

    Solved Tasks 9, 17, 18, 19, 20, 22

---
+++ Task19/Program.cs
@@ -0,0 +1,25 @@
+﻿bool isNumber = false;
+int number = 0;
+
+while (!isNumber)
+{
+    Console.Write("Введите число: ");
+    isNumber = Int32.TryParse(Console.ReadLine(), out number);
+}
+
+bool isPalindrome(int num)
+{
+    int reverseNumber = 0;
+    int numberTemp = num;
+
+    while (numberTemp != 0)
+    {
+        reverseNumber = reverseNumber * 10 + numberTemp % 10;
+        numberTemp /= 10;
+    }
+
+    return num == reverseNumber ? true : false;
+}
+
+string message = isPalindrome(number) ? $"Число {number} является палиндромом" : $"Число {number} не является палиндромом";
+Console.WriteLine(message);
