76a59f4705d7c5f5900ccf01b5c79653101f8aa2
Ivan Sizov <wuller1@gmail.com>
2022-11-22 22:46:40 +1000

    Changed solution for Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -3,8 +3,23 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+// Console.Write("Введите число: ");
+// string stringNumber = Console.ReadLine();
+// var number = Convert.ToInt32(stringNumber);
+
+// if (number < 100)
+// {
+//     Console.WriteLine($"В числе {number} нет третьей цифры");
+// }
+// else
+// {
+//     Console.WriteLine($"Третья цифра числа {number} - {stringNumber[2]}");
+// }
+
 Console.Write("Введите число: ");
 var number = Convert.ToInt32(Console.ReadLine());
+int tempNumber = number;
+int thirdDigit = 0;
 
 if (number < 100)
 {
@@ -12,7 +27,15 @@ if (number < 100)
 }
 else
 {
-    Console.WriteLine($"Третья цифра числа {number} - {Convert.ToString(number)[2]}");
-}
+    while (tempNumber > 99)
+    {
+        if (tempNumber < 1000)
+        {
+            thirdDigit = tempNumber % 10;
+        }
+        tempNumber /= 10;
+    }
 
+    Console.WriteLine($"Третья цифра числа {number} - {thirdDigit}");
+}
 
