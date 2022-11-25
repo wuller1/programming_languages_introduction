389fe44780211fe5c22c9b6ad9d8b712bf0b5d30
Ivan Sizov <wuller1@gmail.com>
2022-11-26 08:43:59 +1000

    solved Task32

---
+++ Task32/Program.cs
@@ -0,0 +1,41 @@
+﻿int[] RandomArray(int numOfElements, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[] array = new int[numOfElements];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1)
+        {
+            Console.Write($"{array[i]}");
+        }
+        else
+        {
+            Console.Write($"{array[i]}, ");
+        }
+    }
+    Console.Write("]\n");
+}
+
+int[] InverseArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] *= (-1);
+    }
+    return arr;
+}
+
+var array = RandomArray(12, -100, 100);
+PrintArray(array);
+var inversedArray = InverseArray(array);
+PrintArray(inversedArray);
