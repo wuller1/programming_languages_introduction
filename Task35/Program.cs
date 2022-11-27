c14a9e1f1bdf7c200981bfeb4429967cc64e43ff
Ivan Sizov <wuller1@gmail.com>
2022-11-27 13:57:58 +1000

    solved Tasks 35, 37

---
+++ Task35/Program.cs
@@ -0,0 +1,41 @@
+﻿int[] RandomArray(int numOfElements, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[] array = new int[numOfElements];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1)
+        {
+            Console.Write($"{array[i]}");
+        }
+        else
+        {
+            Console.Write($"{array[i]}, ");
+        }
+    }
+    Console.Write("]\n");
+}
+
+int NumberOfTwoDigitNumbers(int[] arr)
+{
+    int num = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99) num += 1;
+    }
+    return num;
+}
+
+var array = RandomArray(123, 10, 99);
+PrintArray(array);
+Console.WriteLine(NumberOfTwoDigitNumbers(array));
---
+++ Task37/Program.cs
@@ -0,0 +1,45 @@
+﻿int[] RandomArray(int numOfElements, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[] array = new int[numOfElements];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1)
+        {
+            Console.Write($"{array[i]}");
+        }
+        else
+        {
+            Console.Write($"{array[i]}, ");
+        }
+    }
+    Console.Write("]\n");
+}
+
+int[] ProductOfPairs(int[] arr)
+{
+    int productLength = arr.Length % 2 == 0 ? arr.Length / 2 : arr.Length / 2 + 1;
+    int[] products = new int[productLength];
+
+    for (int i = 0; i < productLength; i++)
+    {
+        if (i == arr.Length - i - 1) products[i] = arr[i];
+        else products[i] = arr[i] * arr[arr.Length - i - 1];
+    }
+    return products;
+}
+
+var array = RandomArray(10, 0, 10);
+PrintArray(array);
+var productArray = ProductOfPairs(array);
+PrintArray(productArray);
