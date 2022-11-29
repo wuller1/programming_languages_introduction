f9c443829594711ae04b40d7d11b068cf8e6dfab
Ivan Sizov <wuller1@gmail.com>
2022-11-30 00:04:45 +1000

    solved task 42

---
+++ Task42/Program.cs
@@ -0,0 +1,12 @@
+﻿string ToBinary(int num)
+{
+    string binary = string.Empty;
+    while (num != 0)
+    {
+        binary = $"{num % 2}{binary}";
+        num /= 2;
+    }
+    return binary;
+}
+
+Console.WriteLine(ToBinary(2));
