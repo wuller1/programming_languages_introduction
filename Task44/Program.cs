9b71b1ffe36b866fc3b9b929e41510d4474c4a8d
Ivan Sizov <wuller1@gmail.com>
2022-11-30 22:14:17 +1000

    Solved Tasks 44, 45

---
+++ Task44/Program.cs
@@ -0,0 +1,22 @@
+﻿void PrintArray(int[] arr)
+{
+    foreach (var num in arr)
+    {
+        Console.Write($" {num}");
+    }
+    Console.WriteLine();
+}
+
+int[] FebonacciSequence(int count)
+{
+    int[] arr = new int[count];
+    for (var i = 0; i < count; i++)
+    {
+        if (i == 0) arr[i] = 0;
+        else if (i == 1) arr[i] = 1;
+        else arr[i] = arr[i - 2] + arr[i - 1];
+    }
+    return arr;
+}
+
+PrintArray(FebonacciSequence(7));
---
+++ Task45/Program.cs
@@ -0,0 +1,24 @@
+﻿void PrintArray(int[] arr)
+{
+    foreach (var num in arr)
+    {
+        Console.Write($" {num}");
+    }
+    Console.WriteLine();
+}
+
+int[] CopyArray(int[] arr)
+{
+    int[] arrCopy = new int[arr.Length];
+    for (var i = 0; i < arr.Length; i++)
+    {
+        arrCopy[i] = arr[i];
+    }
+    return arrCopy;
+}
+
+int[] array = new int[] { 0, 5, 8, 12, 18, 34 };
+PrintArray(array);
+int[] arrayCopy = CopyArray(array);
+arrayCopy[3] = 212;
+PrintArray(arrayCopy);
