bc0a8b6b8eeecc213499c015b74e68cbe0f8c03d
Ivan Sizov <wuller1@gmail.com>
2022-12-05 21:14:05 +1000

    Solved Tasks 47, 50, 52

---
+++ Task50/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 50.Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7->такого элемента в массиве нет
+
+void PrintMatrix(int[,] matrix)
+{
+    for (var i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (var j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] GenerateRandomIntMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (var i = 0; i < rows; i++)
+    {
+        for (var j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+bool InArray(int[,] mat, int row, int column)
+{
+    if (mat.GetLength(0) > row && mat.GetLength(1) > column)
+        return true;
+    return false;
+}
+
+void PrintElementByIndex(int[,] arr)
+{
+    Console.Write("Введите индекс строки: ");
+    int row = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите индекс столбца: ");
+    int column = Convert.ToInt32(Console.ReadLine());
+
+    if (InArray(arr, row, column))
+    {
+        Console.WriteLine($"Элемент с индексами ({row}, {column}) в заданном массиве: {arr[row, column]}");
+    }
+    else
+    {
+        Console.WriteLine($"Элемент с индексами ({row}, {column}) отсутствует в заданном массиве.");
+    }
+}
+
+int[,] matrix = GenerateRandomIntMatrix(3, 4, 0, 9);
+PrintMatrix(matrix);
+PrintElementByIndex(matrix);
---
+++ Task52/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void PrintArray(double[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1)
+        {
+            Console.Write($"{array[i]}");
+        }
+        else
+        {
+            Console.Write($"{array[i]}, ");
+        }
+    }
+    Console.Write("]\n");
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (var i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (var j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] GenerateRandomIntMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (var i = 0; i < rows; i++)
+    {
+        for (var j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+double[] MatrixColumnsMean(int[,] mat)
+{
+    int columnsCount = mat.GetLength(1);
+    double[] columnsSum = new double[columnsCount];
+    double[] columnsMean = new double[columnsCount];
+    for (var j = 0; j < columnsCount; j++)
+    {
+        for (var i = 0; i < mat.GetLength(0); i++)
+        {
+            columnsSum[j] += mat[i, j];
+        }
+        columnsMean[j] = Math.Round(columnsSum[j] / (columnsCount - 1), 1);
+    }
+    return columnsMean;
+}
+
+var matrix = GenerateRandomIntMatrix(3, 4, 0, 9);
+PrintMatrix(matrix);
+var meanArray = MatrixColumnsMean(matrix);
+Console.WriteLine("Среднее арифметическое столбцов:");
+PrintArray(meanArray);
