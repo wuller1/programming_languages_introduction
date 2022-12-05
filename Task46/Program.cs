2a38a333767699ec44b9a925e41d49d7d174047e
Ivan Sizov <wuller1@gmail.com>
2022-12-05 16:43:46 +1000

    solved Task43

---
+++ Task46/Program.cs
@@ -0,0 +1,27 @@
+﻿int[,] GenerateRandomIntMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (var i = 0; i < rows; i++)
+    {
+        for (var j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (var i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (var j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(GenerateRandomIntMatrix(3, 4, -100, 100));
