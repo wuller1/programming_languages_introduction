9967393a0fe637898b4de804cb8d4037e373758d
Ivan Sizov <wuller1@gmail.com>
2022-12-06 23:36:57 +1000

    Solved Task53

---
+++ Task53/Program.cs
@@ -0,0 +1,43 @@
+﻿void PrintMatrix(int[,] matrix)
+{
+    for (var i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (var j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] GenerateRandomIntMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (var i = 0; i < rows; i++)
+    {
+        for (var j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void SwapFirstAndLastRows(int[,] arr)
+{
+    int temp;
+    for (var i = 0; i < arr.GetLength(1); i++)
+    {
+        temp = arr[0, i];
+        arr[0, i] = arr[arr.GetLength(0) - 1, i];
+        arr[arr.GetLength(0) - 1, i] = temp;
+    }
+}
+
+var array = GenerateRandomIntMatrix(3, 4, 0, 9);
+PrintMatrix(array);
+SwapFirstAndLastRows(array);
+Console.WriteLine();
+PrintMatrix(array);
