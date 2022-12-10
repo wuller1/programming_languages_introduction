e46830b1b4b063fde5042f9c52b1e704425ba313
Ivan Sizov <wuller1@gmail.com>
2022-12-10 23:13:55 +1000

    Solved tasks 63, 65, 67, 69

---
+++ Task67/Program.cs
@@ -0,0 +1,10 @@
+﻿int SumOfDigits(int number, int sum = 0)
+{
+    if (number != 0)
+    {
+        return number % 10 + SumOfDigits(number / 10, sum);
+    }
+    return 0;
+}
+
+Console.WriteLine(SumOfDigits(1534));
